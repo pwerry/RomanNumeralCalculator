a95238884f3ff8839a509458282fbfaccf417e30
Christopher Stott <christopherstott@gmail.com>
2016-12-29 01:07:45 -0800

    Introduce assert failure

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -24,6 +24,6 @@ class RomanNumeralCalculatorTests: XCTestCase {
     func testExample() {
         XCTAssertEqual(toRoman(1), "I")
         XCTAssertEqual(toRoman(2), "II")
-        XCTAssertEqual(toRoman(4), "IV")
+        XCTAssertEqual(toRoman(4), "IVx")
     }
 }
