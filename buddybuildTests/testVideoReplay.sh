b08a7d4799292ddd43736b8b7ff34d109391cbe1
Peter Werry <pwerry@gmail.com>
2017-10-16 13:25:26 -0700

    bring back the magic

--- buddybuildTests/testVideoReplay.sh
+++ buddybuildTests/testVideoReplay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-#cd /tmp/sandbox/$BUDDYBUILD_BUILD_ID
-#VIDEO_REPLAYS=$(find . -name '*.mp4')
-#if [$VIDEO_REPLAYS == ""]; then
-#   echo "Test failed: could not find any Video Replays."
-#   exit -1;
-#else
-#   echo "Video Replays $VIDEO_REPLAYS."
-#fi
+cd /tmp/sandbox/$BUDDYBUILD_BUILD_ID
+VIDEO_REPLAYS=$(find . -name '*.mp4')
+if [$VIDEO_REPLAYS == ""]; then
+   echo "Test failed: could not find any Video Replays."
+   exit -1;
+else
+   echo "Video Replays $VIDEO_REPLAYS."
+fi
